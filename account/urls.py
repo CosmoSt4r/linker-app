7399935fb780c97a18a6128084b213349c702c7e
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-08-15 18:05:26 +0500

    'account' app added

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+app_name = 'account'
+urlpatterns = [
+    # path('', <base_view>, name="base")
+    # path('login/', <login_view>, name='login'),
+    # path('signup/', <signup_view>, name='signup'),
+    # path('home/', <home_view>, name='home'),
+]
