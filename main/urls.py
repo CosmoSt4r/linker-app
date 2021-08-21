492ed7058b6088de84644d0dfe4528b4d5b23994
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-08-21 10:49:29 +0500

    'main' app initialized

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "main-page"
+urlpatterns = [
+    # path("", views.main_view, name="main"),
+    # path("user/<str:username>", views.show_user, name="show-user"),
+]
