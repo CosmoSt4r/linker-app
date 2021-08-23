6b10378bb67062c07869ea4a76138de9b4799d67
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-08-23 15:15:05 +0500

    Error pages added

--- linker/settings.py
+++ linker/settings.py
@@ -22,11 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 123
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEBUG")
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
