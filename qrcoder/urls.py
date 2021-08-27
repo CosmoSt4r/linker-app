759b6afc5c615613c03c99d8f5629063846b676a
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-08-27 14:29:47 +0500

    Project renamed to QRCoder

--- qrcoder/urls.py
+++ qrcoder/urls.py
@@ -1,4 +1,4 @@
-"""linker URL Configuration
+"""qrcoder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
